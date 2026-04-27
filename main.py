0a288f732c5a504bb56907ca2d76e4f6712e1923
Fragile <xyorhzhf@gmail.com>
2026-04-28 01:07:25 +0900

    add chain

--- main.py
+++ main.py
@@ -1,10 +1,41 @@
-from dotevn import load_dotenv
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_ollama import ChatOllama
+
 
 load_dotenv()
 
+
 def main():
     print("Hello from langchain-course!")
+    information = """
+        일론 리브 머스크(영어: Elon Reeve Musk, 1971년 6월 28일~)는 남아프리카 공화국 출신 미국의 기업인, 정치인, 투자자이다. 페이팔의 전신이 된 온라인 결제 서비스 회사 X.com, 민간 우주기업 스페이스X를 창립했고, 전기자동차 기업 테슬라의 회장이기도 하다.
+        남아프리카 공화국 프리토리아의 부유한 가정에서 태어난 머스크는 1989년 캐나다로 이민을 갔다. 그의 어머니가 캐나다에서 태어났기 때문에 그는 캐나다 시민권을 가지고 있다. 그는 1997년 미국 필라델피아의 펜실베이니아 대학교에서 학사 학위를 받은 후, 사업을 추구하기 위해 캘리포니아주로 이주했다. 1995년, 머스크는 소프트웨어 회사 Zip2를 공동 설립했다. 1999년 매각 후, 그는 온라인 결제 회사 X.com을 공동 설립했으며, 이 회사는 나중에 합병하여 페이팔을 형성했고, 페이팔은 2002년 이베이에 인수되었다. 머스크는 2002년 미국 시민권을 취득했다.
+        2002년, 머스크는 우주 기술 회사 스페이스X를 설립하여 CEO이자 최고 엔지니어가 되었다. 이 회사는 이후 재사용 로켓 및 상업 우주 비행 분야에서 혁신을 이끌었다. 머스크는 2004년 초기 투자자로 자동차 제조업체 테슬라에 합류하여 2008년 CEO이자 제품 설계자가 되었고, 이후 전기차 분야의 선두 주자가 되었다. 2015년, 그는 인공지능 (AI) 연구를 발전시키기 위해 오픈AI를 공동 설립했으나, 나중에 떠났다. 조직의 방향과 2020년대 AI 붐에서의 리더십에 대한 불만이 커지면서 그는 xAI를 설립하게 되었다. 2022년, 그는 소셜 네트워크 트위터를 인수하여 상당한 변화를 구현했으며, 2023년에는 이를 X로 리브랜딩했다. 그의 다른 사업으로는 2016년에 공동 설립한 뇌 과학 회사 뉴럴링크와 2017년에 설립한 터널링 회사 더 보링 컴퍼니가 있다. 2025년 11월, 머스크에게 1조 달러 상당의 테슬라 보상 패키지가 승인되었으며, 그는 특정 목표를 달성하면 10년에 걸쳐 이를 받을 예정이다.
+        머스크는 2024년 미국 대통령 선거에서 도널드 트럼프를 지지하는 최대 기부자였다. 트럼프가 2025년 초 대통령에 취임한 후, 머스크는 미국 대통령 선임 고문으로 재직했으며, 정부효율부 (DOGE)의 사실상의 수장으로 활동했다. 그러나 트럼프와의 공개적인 갈등 후, 머스크는 트럼프 행정부를 떠나 자신의 회사 경영으로 돌아왔다.
+        머스크는 세계적인 극우 인사, 대의, 정당의 지지자로 나서고 있으며, 그의 정치 활동, 견해 및 발언은 그를 논란의 인물로 만들었다. 머스크는 코로나19 팬데믹에 관한 허위 사실, 음모론 조장, 인종주의적 발언 등으로 비판받았다. 그의 트위터 인수는 증오 발언의 증가와 서비스 상의 허위 정보 확산으로 인해 논란이 되었는데, 이는 검열을 줄이겠다는 그의 공약에 따른 것이었다. 그의 도널드 트럼프 2기 행정부에서의 활동, 특히 DOGE에서의 역할은 대중적 반발을 일으켰다.
+    """
 
+    summary_template = f"""
+    given the information about a person I want you to create:
+    1. A short summary
+    2. two interesting facts about them
+    3. please answer in korean
+    
+    information: {information} 
+    """
+
+    summary_prompt_template = PromptTemplate(
+        input_variables=["information"], template=summary_template
+    )
+
+    llm = ChatGoogleGenerativeAI(temperature=0, model="gemini-2.5-flash-lite")
+    # llm = ChatOllama(temperature=0, model="gemma3:270m")
+    chain = summary_prompt_template | llm
+
+    response = chain.invoke(input={"information": information})
+    print(response.content)
 
 if __name__ == "__main__":
     main()
